f60f209fc30710ba1788e6f0c815e07f95196c69
Alex-Rockwell <vin-ag@list.ru>
2022-03-17 22:20:04 +0400

    part1

---
+++ src/Components/PostItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="post">
+    <div>
+        <p>{{ post.id }}</p>
+        <p><strong>Название: </strong>{{ post.title }}</p>
+        <p><strong>Описание: </strong>{{ post.body}}</p>
+    </div>
+    <div class="post__btns">
+      <my-button @click="$emit('remove', post)" >Delete</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      post: {
+        type: Object,
+        required: true,
+      },
+    },
+  }
+</script>
+
+<style lang="css" scoped>
+  .post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
